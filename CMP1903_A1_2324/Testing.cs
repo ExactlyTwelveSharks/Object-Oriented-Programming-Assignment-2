4475a2ad380cf56164c7857e488a0bc66e0beaae
ExactlyTwelveSharks <158062076+ExactlyTwelveSharks@users.noreply.github.com>
2024-04-25 19:10:50 +0100

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -3,34 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace CMP1903_A1_2324
+namespace Object_Oriented_Programming_Assignment_2
 {
-    internal class Testing
+    class Testing
     {
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
-
-public Testing(Game Play)
-{
-    Console.WriteLine("Below is a test game to verify that everything works as intended.");
-    TestGame(Play);
-}
-public static void TestGame(Game Play)
-{
-    int TestDiceSum = Play.GetDiceRolls();
-    Die DieRoll = new Die();
-    Debug.Assert(TestDiceSum >= 3 && TestDiceSum <= 18, "The sum is an invalid result, out of range.");
-    for (int i = 0; i < 50; i++)
-    {
-        int TestRolls = DieRoll.Roll();
-        Debug.Assert(TestRolls >= 1 && TestRolls <= 6, "A die has given an invalid result, The dice's range isn't correct.");
+        static void Main(string[] args)
+        {
+            SevensOut SevensOutGame = new SevensOut();
+            ThreeOrMore ThreeOrMoreGame = new ThreeOrMore();
+            Statistics ShowStatistics = new Statistics();
+            int SevensOutPlayed = 0;
+            int ThreeOrMorePlayed = 0;
+            Game Play = new Game();
+            bool PlayTime = true;
+            int TestDiceSum = Play.GetDiceRolls();
+            Die DieRoll = new Die();
+            Debug.Assert(TestDiceSum >= 3 && TestDiceSum <= 18, "The sum is an invalid result, out of range.");
+            for (int i = 0; i < 50; i++)
+            {
+                int TestRolls = DieRoll.Roll();
+                Debug.Assert(TestRolls >= 1 && TestRolls <= 6, "A die has given an invalid result, The dice's range isn't correct.");
+            }
+            int SevensOutScore = 0;
+            int ThreeOrMoreScore = 0;
+            while (PlayTime == true)
+            {
+                
+                Console.WriteLine("Would you like to play a dice game? Please type either SevensOut or ThreeOrMore. If you do not wish to play a game, type anything else. You can check your statistics by typing Statistics.");
+                string GamePlayed = Console.ReadLine();
+                if (GamePlayed == "SevensOut")
+                {
+                    SevensOutScore = SevensOutGame.PlaySevensOut();
+                    SevensOutPlayed++;
+                }
+                else if (GamePlayed == "ThreeOrMore")
+                {
+                    ThreeOrMoreScore = ThreeOrMoreGame.PlayThreeOrMore();
+                    ThreeOrMorePlayed++;
+                }
+                else if (GamePlayed == "Statistics")
+                { ShowStatistics.ShowStats(SevensOutPlayed, ThreeOrMorePlayed, SevensOutScore, ThreeOrMoreScore); }
+                else
+                { return; }
+            }
+        }
     }
-    Console.ReadLine();
-}
-    }
-}
